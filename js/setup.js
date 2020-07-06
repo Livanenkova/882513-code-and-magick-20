ee70118a0d62ba74e7fdab9e6707a9dcb2bcdc73
Livanenkova <arina909@yandex.ru>
2020-07-06 15:04:38 +0300

    do home work

--- js/setup.js
+++ js/setup.js
@@ -6,6 +6,7 @@ var SECOND_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Ва
 var COATS_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)',
   'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
 
 var userDialog = document.querySelector('.setup');
@@ -52,3 +53,123 @@ function renderWizards() {
 }
 
 renderWizards();
+
+// module4-task1
+
+var setupOpen = document.querySelector('.setup-open');
+var setup = document.querySelector('.setup');
+var setupClose = setup.querySelector('.setup-close');
+var setupIcon = document.querySelector('.setup-open-icon');
+var setupwizardCoat = document.querySelector('.wizard-coat');
+var setupWizardFireball = document.querySelector('.setup-fireball-wrap');
+var setupWizardEyes = document.querySelector('.wizard-eyes');
+var setupWizardCoatInput = document.querySelector('input[name="coat-color"]');
+var setupWizardEyesInput = document.querySelector('input[name="eyes-color"]');
+var setupWizardFireballInput = document.querySelector('input[name="fireball-color"]');
+var MIN_NAME_LENGTH = 2;
+var MAX_NAME_LENGTH = 25;
+
+var onPopupEscPress = function (evt) {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  setup.classList.remove('hidden');
+
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function () {
+  setup.classList.add('hidden');
+
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.key === 'Enter') {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.key === 'Enter') {
+    closePopup();
+  }
+});
+
+setupIcon.addEventListener('focus', function (evt) {
+  if (evt.key === 'Enter') {
+    openPopup();
+  }
+});
+
+var userNameInput = document.querySelector('.setup-user-name');
+
+userNameInput.addEventListener('invalid', function () {
+  if (userNameInput.validity.valueMissing) {
+    userNameInput.setCustomValidity('Обязательное поле');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+userNameInput.addEventListener('input', function () {
+  var valueLength = userNameInput.value.length;
+
+  if (valueLength < MIN_NAME_LENGTH) {
+    userNameInput.setCustomValidity('Ещё ' + (MIN_NAME_LENGTH - valueLength) + ' симв.');
+  } else if (valueLength > MAX_NAME_LENGTH) {
+    userNameInput.setCustomValidity('Удалите лишние ' + (valueLength - MAX_NAME_LENGTH) + ' симв.');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+var renderRandomColor = function (element, input, color) {
+  element.style.fill = color;
+  input.value = color;
+};
+
+var renderRandomBackGroundColor = function (element, input, backgroundColor) {
+  element.style.backgroundColor = backgroundColor;
+  input.value = backgroundColor;
+};
+
+setupWizardEyes.addEventListener('click', function () {
+  onChangeEyeColor();
+});
+
+var onChangeEyeColor = function () {
+  var color = getRandomValueFromArray(EYES_COLOR);
+  renderRandomColor(setupWizardEyes, setupWizardEyesInput, color);
+};
+
+setupWizardFireball.addEventListener('click', function () {
+  onChangeFireballColor();
+});
+
+
+var onChangeFireballColor = function () {
+  var color = getRandomValueFromArray(FIREBALL_COLORS);
+  renderRandomBackGroundColor(setupWizardFireball, setupWizardFireballInput, color);
+};
+
+setupwizardCoat.addEventListener('click', function () {
+  onChangeCoatColor();
+});
+
+var onChangeCoatColor = function () {
+  var color = getRandomValueFromArray(COATS_COLOR);
+  renderRandomColor(setupwizardCoat, setupWizardCoatInput, color);
+};
